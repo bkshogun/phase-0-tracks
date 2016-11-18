442295c7270505c7089daac90d33765414a981d4
Michael de Gaetano <oxmikeyd@yahoo.com>
2016-11-18 04:23:35 -0500

    Cleaned up conditional statements

--- vampires.rb
+++ vampires.rb
@@ -5,23 +5,36 @@ puts "How old are you? What year were you born?"
 user_age = gets.to_i
 year_born = gets.to_i
 
-puts "Our company cafeteria serves garlic bread. Should we order some for you?"
+puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 garlic_bread = gets.chomp
+if garlic_bread == "y"
+	garlic_bread = true
+	garlic_result = "would like to order garlic bread"
+else
+	garlic_bread = false
+	garlic_result = "would not like to order garlic bread"
+end
 
-puts "Would you like to enroll in the company's health insurance?"
+puts "Would you like to enroll in the company's health insurance? (y/n)"
 insurance = gets.chomp
+if insurance == "y"
+	insurance = true
+	insurance_result = "would like to enroll in company insurance"
+else
+	insurance = false
+	insurance_result = "would not not like to enroll in comapny insurance"
+end
 
 verify_age = (2016 - year_born)
 
-if (user_age == verify_age) && (garlic_bread || insurance)
-	puts "Probably not a vampire"
+if vamp_name == ("Drake Cula" || "Tu Fang")
+	puts "Applicant's age, galirc and insurance preferences are irrelevent because his name is #{vamp_name} and he is definitely a vampire"
+elsif (user_age == verify_age) && (garlic_bread || insurance)
+	puts "Applicant #{vamp_name} got his age right, #{garlic_result}, #{insurance_result} and is probably not a vampire"
+elsif (user_age != verify_age) && (!garlic_bread && !insurance)
+	puts "Applicant #{vamp_name} got his age wrong, #{garlic_result}, #{insurance_result} and is almost certainly a vampire"
 elsif (user_age != verify_age) && (!garlic_bread || !insurance)
-	puts "Probably a vampire"
-elsif (user_age != verify_age) && !garlic_bread && !insurance
-	puts "Almost certainly a vmapire"
-elsif name == "Drake Cula" || "Tu Fang"
-	puts "Definitely a vampire"
+	puts "Applicant #{vamp_name} got his age wrong, #{garlic_result}, #{insurance_result} and is probably a vampire"
 else 
 	puts "Results inconclusive"
-end 
-		
+end 		
