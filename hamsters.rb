54b2d17d4f41a13abddf2206b6dc416b54673cea
Michael de Gaetano <oxmikeyd@yahoo.com>
2016-11-18 02:36:27 -0500

    Final commit

--- hamsters.rb
+++ hamsters.rb
@@ -8,28 +8,20 @@ puts "Enter fur color:"
 fur_color = gets.chomp
 
 puts "Do you think #{hamster_name} is a good canidate for adoption? (y/n)"
-good_can = gets.chomp
-	if good_can == "y"
-		adopt = true
-	elsif good_can == "n"
-		adopt = false
-	end
-	
-=begin 
 adopt = gets.chomp
-	if adopt == "y"
+	if adopt == "y" or "yes"
 		adopt = "a good"
 	else 
 		adopt = "not a good"
 	end
-=end 
 
 puts "How old is #{hamster_name}?"
-hamster_age = gets.to_i
+hamster_age = gets.chomp
 	if hamster_age == ""
 		hamster_age = nil
+	else 	
 	end
 
-puts "So, #{hamster_name} puts out a volume level of #{max_vol} and has
+print "So, #{hamster_name} puts out a volume level of #{max_vol} out of 10 and has
 #{fur_color} fur. You feel that #{hamster_name} is #{adopt} candidate for adoption 
 at #{hamster_age} years old."
