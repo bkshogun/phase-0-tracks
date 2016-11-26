4007b4586fdaf726d735f520005d0058d2847076
Michael de Gaetano <oxmikeyd@yahoo.com>
2016-11-26 04:01:06 -0500

    Fixed logic

--- nested_data_structures.rb
+++ nested_data_structures.rb
@@ -1,44 +1,31 @@
+nl = {
+  name: "National League",
+  nl_east: {
+    name: "National League East",
+    mets: ["Syndergaard", "Harvey", "deGrom"],
+    nationals: [],
+    braves: [],
+    marlins: [],
+    phillies: []
+  },
+  nl_central: {
+    name: "National League Central",
+      cubs: ["Arrieta", "Lester"],
+      reds: [],
+      brewers: [],
+      cardinals: [],
+      pirates: []
+    },
+  nl_west: {
+    name: "National League West",
+    giants: [],
+    padres: []
+  }
+  }
 
-major_league_baseball = {
-   name: 'Major League Baseball', 
-   leagues: {
-   	   national_league: {
-   	   name: 'National League',
-          nl_east: {
-             name: 'NL East',
-      	     teams: {
-      	     	new_york_mets: ['Syndergaard' 'Harvey' 'deGrom' 'Matz' 'Colon'],
-      	        washington_nationals: ['Scherzer', 'Strasburg', 'Gonzalez', 'Roark', 'Ross'],
-      	        atlanta_braves: ['Tehran', 'Dickey', 'Foltynewicz', 'Wisler', 'Blair'],
-      	        miami_marlins: ['Conley', 'Chen', 'Koehler', 'Urena', 'Esch'],
-      	        philadelphia_phillies: ['Nola', 'Hellickson', 'Eflin', 'Thompson', 'Morgan']
-      	        },
-      	     },
-          nl_central: {
-             name: 'NL Central',
-             teams: {
-                chicago_cubs: [],
-                cincinatti_reds: [],
-                milwaukee_brewers: [],
-                st_louis_cardinlas: [],
-                pittsburgh_pirates: []
-                },
-             },
-          nl_west: {
-             name: 'NL West',
-             teams: {
-             	san_francisco_giants: [],
-                san_diego_padres: [],
-                los_angeles_dodgers: [],
-                arizona_diamondbacks: [],
-                colorado_rockies: []
-                },
-             },   
-        },     
-        #american_league: {
-        #name: 'American League' [],
-        #}
-    }
-}
 
-puts major_league_baseball[:leagues][:national_league][:nl_east][:new_york_mets][2]
+puts nl[:nl_east][:mets][0]
+puts nl[:name]
+nl[:nl_east][:mets] << "Lugo"
+puts nl[:nl_east][:mets]
+puts nl[:nl_central][:cubs][0]
