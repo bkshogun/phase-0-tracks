f7d6501459d2025b284ed0a6be379ecc3c5a75f2
Michael de Gaetano <oxmikeyd@yahoo.com>
2016-11-26 00:38:33 -0500

    Initial commit

---
+++ nested_data_structures.rb
@@ -0,0 +1,33 @@
+
+major_league_baseball: {
+   name: 'Major League Baseball', 
+   leagues: {
+   	   national_league: {
+   	   name: 'National League'
+          nl_east:{
+          name: 'NL East',
+      	     new_york_mets: ['Syndergaard', 'Harvey', 'deGrom', 'Matz', 'Colon'],
+      	     washington_nationals: ['Scherzer', 'Strasburg', 'Gonzalez', 'Roark', 'Ross'],
+      	     atlanta_braves: ['Tehran', 'Dickey', 'Foltynewicz', 'Wisler', 'Blair'],
+      	     miami_marlins: ['Conley', 'Chen', 'Koehler', 'Urena', 'Esch'],
+      	     philadelphia_phillies: ['Nola', 'Hellickson', 'Eflin', 'Thompson', 'Morgan']
+      	     }
+          nl_central: {
+             name: 'NL Central'
+             chicago_cubs: [],
+             cincinatti_reds: [],
+             milwaukee_brewers: [],
+             st_louis_cardinlas: [],
+             pittsburgh_pirates: []
+             }
+          nl_west: {
+             san_francisco_giants: [],
+             san_diego_padres: [],
+             los_angeles_dodgers: [],
+             arizona_diamondbacks: [],
+             colorado_rockies: []
+        name: 'American League'
+
+      }
+	}
+}
