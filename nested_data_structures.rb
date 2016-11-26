a1ec9e45aac735632feb8b542d76dae60315b9b5
Michael de Gaetano <oxmikeyd@yahoo.com>
2016-11-26 04:06:39 -0500

    Added pitchers in team arrays

--- nested_data_structures.rb
+++ nested_data_structures.rb
@@ -2,30 +2,26 @@ nl = {
   name: "National League",
   nl_east: {
     name: "National League East",
-    mets: ["Syndergaard", "Harvey", "deGrom"],
-    nationals: [],
-    braves: [],
-    marlins: [],
-    phillies: []
+    mets: ["Syndergaard", "Harvey", "deGrom", "Wheeler", "Matz"],
+    nationals: ["Scherzer", "Strasburg", "Gonzalez", "Roark", "Ross"],
+    braves: ["Tehran", "Dickey", "Foltynewicz", "Wisler", "Blair"],
+    marlins: ["Conley", "Chen", "Koehler", "Urena", "Esch"],
+    phillies: ["Nola", "Hellickson", "Eickoff", "Eflin", "Thompson"]
   },
   nl_central: {
     name: "National League Central",
-      cubs: ["Arrieta", "Lester"],
+      cubs: ["Arrieta", "Lester", "Hendricks", "Lackey"],
       reds: [],
       brewers: [],
       cardinals: [],
       pirates: []
-    },
+  },
   nl_west: {
     name: "National League West",
     giants: [],
-    padres: []
+    padres: [],
+    dodgers: ["Kershaw", "Maeda", "Urias"],
+    diamondbacks: [],
+    rockies: []
   }
-  }
-
-
-puts nl[:nl_east][:mets][0]
-puts nl[:name]
-nl[:nl_east][:mets] << "Lugo"
-puts nl[:nl_east][:mets]
-puts nl[:nl_central][:cubs][0]
+}
