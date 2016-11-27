953fc08d86cb4d2d927b266ffac628c789b76207
Michael de Gaetano <oxmikeyd@yahoo.com>
2016-11-27 09:29:02 -0500

    Added first iteration

---
+++ iteration.rb
@@ -0,0 +1,27 @@
+
+# Rocky Films
+=begin 
+rocky_films = {1 => "Apollo Creed", 
+	2 => "Apollo Creed (again)", 
+	3 => "Clubber Lang", 
+	4 => "Ivan Drago", 
+	5 => "Tommy Gunn"
+}
+
+# modify code, change character to actor or the other way around, 
+# then print again
+
+rocky_films.each do |film_num, bad_guy|
+	puts "In Rocky #{film_num}, Rocky Balboa is the good guy, and #{bad_guy} is the bad guy"
+end 
+=end 
+
+greek_let = ["alpha", "beta", "gamma", "delta", "epsilon"]
+puts "Original data:"
+p greek_let
+
+greek_let.each do |letter|
+
+p letter.length 
+end 
+
