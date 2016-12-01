696534478c8ab66f5795fabec60523099e0402e7
Michael de Gaetano <oxmikeyd@yahoo.com>
2016-12-01 18:34:21 -0500

    Incorrect fib sequence output

--- problem_solving_methods.rb
+++ problem_solving_methods.rb
@@ -1,28 +1,19 @@
 arr = [42, 89, 23, 1]
 
-def search_array(arr, y)
-   arr.each do |num|
-   	if num == y
-   	   return arr.index?
-   	else return nil 
-   	end 
-    end 
-end
-
-p search_array(arr, 89)
-
-
-# 
-
-=begin
-fib_seq = []
-
-def fibb(n)
-if n >= 1
-   n = (n - 1) + (n - 2)
-   fib_seq << n 
-end 
+def search_array(arr, n)
+  n = arr.index(n)
 end 
 
- p fib_seq[6]
-=end 
+search_array(arr, 1)
+
+
+def fib_seq(n)
+   yield * n 
+end 
+
+fib_math do 
+	n = (n - 1) + (n - 2)
+end 
+
+fib_seq(6)
+# => [0, 1, 1, 2, 3, 5]
