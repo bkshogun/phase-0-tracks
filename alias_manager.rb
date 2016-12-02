796b8c3265f267a18c79c97e943ad0a37ed390c1
Michael de Gaetano <oxmikeyd@yahoo.com>
2016-12-02 05:35:37 -0500

    Starting over...

--- alias_manager.rb
+++ alias_manager.rb
@@ -1,30 +1,8 @@
 # Establish a variable with the spy's name
 spy_name = "Felicia Torres"
 
-# Define array of vowels 
+# Define array of vowels and constnants 
 vowels = ["a", "e", "i", "o", "u"]
+constnants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"]
 
-# Then break the string into the individual words, and reverse those words
-# Transpose vowels first, re-join, and then reverse?
-#spy_name = spy_name.split.reverse
-
-# Break up spy name into individual characters in an array
-spy_array = spy_name.chars 
-
-# Check if any charcaters in spy_array are in vowels array
-
-spy_array.any? {|x| ["a", "e", "i", "o", "u"].include?(x)}
-
-# Find corresponding index in vowel array
-
-# Add one to vowel array index
-
-def next_vowel = (vowels.index('#vowel') + 1)
-next_next_vowel = vowels[next_vowel]
-p next_next_vowel
-end
-
-# Match vowel in spy name array to vowel array, then add one to index
-
-p spy_name
-p spy_array 
+constants.length 
