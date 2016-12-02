c303c85f2e8adfa954a4ff814e3d13682ef169b7
Michael de Gaetano <oxmikeyd@yahoo.com>
2016-12-01 20:18:46 -0500

    Initial commit

---
+++ alias_manager.rb
@@ -0,0 +1,7 @@
+#First, I would establish a variable with the spy's name.
+spy_name = "Felicia Torres"
+
+#Then break the string into the individual words
+spy_name = spy_name.split(' ')
+
+puts spy_name
