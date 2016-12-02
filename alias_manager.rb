ef5b0c0ac6fcc491be8e9db87ef4d9d44a46c8f8
Michael de Gaetano <oxmikeyd@yahoo.com>
2016-12-02 00:16:43 -0500

    Added pseudocode

--- alias_manager.rb
+++ alias_manager.rb
@@ -1,7 +1,25 @@
-#First, I would establish a variable with the spy's name.
+# First, I would establish a variable with the spy's name
 spy_name = "Felicia Torres"
 
-#Then break the string into the individual words
-spy_name = spy_name.split(' ')
+# Define array of vowels 
+vowels = ["a", "e", "i", "o", "u"]
 
-puts spy_name
+# Then break the string into the individual words, and reverse those words
+# Transpose vowels first, re-join, and then reverse?
+#spy_name = spy_name.split.reverse
+
+# Break up spy name into individual characters in an array
+spy_array = spy_name.chars 
+
+#if spy_name.include?("a", "e", "i", "o", "u")
+#	spy_name do 
+#puts spy_name
+
+# Find corresponding index in vowel array
+
+# Add one to vowel array index
+
+# Match vowel in spy name array to vowel array, then add one to index
+
+p spy_name
+p spy_array 
