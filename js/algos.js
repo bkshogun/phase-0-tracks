892823ec6581666ee30f85f85129523ef04ee02f
Michael R de Gaetano <oxmikeyd@yahoo.com>
2016-12-12 12:15:35 -0500

    Final attempt

---
+++ js/algos.js
@@ -0,0 +1,45 @@
+// If I'm trying to find the longest string in an array, that would mean the string
+// with the most characters
+
+// Iterate through the array, splitting it into individual strings and Use a function 
+// that counts the letters in a given string
+
+var array1 = ["red", "orange", "green", "blue"];
+
+var array2 = [1, 2, 3, 4];
+
+var array3 = [1, 2, "red", "orange"];
+
+for (var i = 0; i < array1.length; i++) {
+	var strLength = (array1[i].length); 
+ 	array1[i] = strLength;
+	
+}
+console.log (array1)
+// Assign a length to the string and push into a new data structure, similar to a 
+// key/value pair in a hash
+
+// Sort the new data structure by value
+
+// Extract the indexed position, either first or last based on sort order
+
+
+
+// One way, albeit inefficient, would be to add the lengths of two arrays and 
+// return sum1. Then, combine the two arrays but keep only the unique values. 
+// The length of the new array can be sum2. If sum1 equals sum2, there are no
+// duplicated elements in the array, and vise-versa... but just now realizing 
+// that is not what the asignment is asking. 
+
+var matchString = false
+
+//if (... == ...) {
+//	matchString = true
+//} else {
+//	matchString = false
+//}
+
+
+
+
+
