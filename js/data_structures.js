26fc1b9a8ad367825a0378416b124e770c1f8391
Michael R de Gaetano <oxmikeyd@yahoo.com>
2016-12-12 10:07:57 -0500

    Added car constructor

---
+++ js/data_structures.js
@@ -0,0 +1,35 @@
+var horseyColors = ["Red", "Green", "Blue", "Orange"];
+
+var horseyNames = ["Ed", "Elmer", "Horseface", "Jim"];
+
+var horseyCombo = [];
+
+horseyNames.forEach(function(value, index) {
+  horseyCombo[value] = horseyColors[index];
+
+});
+
+console.log(horseyCombo);
+
+
+
+function Car(make, model, copMagnet) {
+	//console.log("My new car:", this);
+	this.make = make;
+	this.model = model;
+	this.copMagnet = copMagnet;
+	this.boom = function() { console.log("BOOM!");};		//figure out how to make these not print
+	this.vroom = function() { console.log("VROOOOOOM");}; 	//nor this one 
+}
+
+console.log("Building car...");
+var Car1 = new Car("Ferrari", 456, true);
+console.log(Car1);
+console.log(Car1.make + " goes...");
+console.log(Car1.vroom());
+
+console.log("Building another car...");
+var Car2 = new Car("Ford", "Pinto", false);
+console.log(Car2);
+console.log(Car2.model + " goes...");
+console.log(Car2.boom());
