f5f0e86b14f684c810493f60590092b923313106
Michael R de Gaetano <oxmikeyd@yahoo.com>
2017-01-02 21:17:37 -0700

    Add files via upload

--- ruby/gps2_2_redo.rb
+++ ruby/gps2_2_redo.rb
@@ -1,1 +1,26 @@
-# GPS 2.2
+def grocery_list(item_string)
+  item_string_split = item_string.split
+  grocery_list_hash = {}
+  item_string_split.each { |i| grocery_list_hash[i] = 2 }
+  grocery_list_hash
+end 
+
+def add_item(existing_hash, new_item, quantity = 2)
+  existing_hash[new_item] = quantity
+  p existing_hash
+end 
+
+def remove_item(existing_hash, item)
+  existing_hash.delete(item)
+end 
+
+def update_quantity(existing_hash, item, quantity)
+  existing_hash[item] = quantity
+end 
+
+existing_hash = grocery_list("carrots apples cereal pizza")
+
+#add_item(existing_hash, "milk")
+#remove_item(existing_hash, "apples")
+#update_quantity(existing_hash, "pizza", 50)
+p existing_hash
